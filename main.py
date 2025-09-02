9d7655b7065a1d38f3bae39a4828cb770bacff2a
coder-Rishi05 <rawatrishi1121@gmail.com>
2025-09-02 21:13:48 +0530

    fixed the function error

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 print("Web scrapping")
 
 import requests
+import os
 
 url = "https://timesofindia.indiatimes.com/india/delhi"
 
-
 def fetchAndSaveToFile(url, path):
     r = requests.get(url)
-    with open(path,"w") as f:
+    # Make sure directory exists
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+    # Write with UTF-8 encoding
+    with open(path, "w", encoding="utf-8") as f:
         f.write(r.text)
-
-
 
 fetchAndSaveToFile(url, "data/times.html")
